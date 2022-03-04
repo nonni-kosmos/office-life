5a581e2ec878ffea5b07a66d94d38856b1a32b0b
nonni-kosmos <jon@kosmosogkaos.is>
2022-03-04 10:32:40 +0000

    wip

--- src/components/Model/Model.tsx
+++ src/components/Model/Model.tsx
@@ -23,8 +23,6 @@ const Model = ({ active, npc }: IModel) => {
 
   object.rotation.set(npc.position[0], npc.position[1], npc.position[2])
 
-  object.rotateOnAxis(new THREE.Vector3(), 90)
-
   const VISIBLE = npc.scale
 
   const { questDialogOpen, npcColor } = useGameStore()
